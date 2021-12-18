d9d1a8e26644d144253f88571ef6fce8eb6b88f3
sachinnabar <sach5979@hotmail.com>
2021-12-18 16:33:57 -0500

    Added csv files and updated schema_sql file.

--- schema_sql.sql
+++ schema_sql.sql
@@ -51,3 +51,137 @@ CREATE TABLE titles (
   FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
   PRIMARY KEY (title)
 );
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO emp_count
+FROM current_emp as ce
+LEFT JOIN department_employees as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+
+SELECT * FROM de.dept_no;
+
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+
+
+SELECT e.emp_no,
+    e.first_name,
+	e.last_name,
+    e.gender,
+    s.salary,
+    de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN department_employees as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01');
+
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+INNER JOIN departments AS d
+ON (dm.dept_no = d.dept_no)
+INNER JOIN current_emp AS ce
+ON (dm.emp_no = ce.emp_no);
+
+
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN department_employees AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+--Sales Team Info
+SELECT ri.emp_no,
+ri.first_name,
+ri.last_name,
+di.dept_name
+INTO sales_info
+FROM retirement_info as ri
+INNER JOIN dept_info AS di
+ON (ri.emp_no = di.emp_no)
+WHERE (di.dept_name = 'Sales');
+
+--Sales & Development Team Info
+SELECT ri.emp_no,
+ri.first_name,
+ri.last_name,
+di.dept_name
+INTO sales_development_info
+FROM retirement_info as ri
+INNER JOIN dept_info AS di
+ON (ri.emp_no = di.emp_no)
+WHERE (di.dept_name = 'Sales' OR di.dept_name = 'Development');
+
+--Create Devliverable 1
+SELECT e.emp_no,
+e.first_name,
+e.last_name,
+ti.title,
+ti.from_date,
+ti.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no;
+
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+ORDER BY emp_no, to_date DESC;
+
+
+--Retrive the number of employees by most recent job title
+SELECT COUNT(title), title
+INTO retiring_titles 
+FROM unique_titles 
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+--Create Devliverable 2
+
+--Employees who are eligible to participate in a mentorship program
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+ti.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN department_employees AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE (birth_date BETWEEN '1965-01-01' AND '1965-12-31') 
+AND (de.to_date = '9999-01-01')
+ORDER BY emp_no;
